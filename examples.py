c976981f58deb2d052446023e2a3ebdb6f59584a
dineshindixpert <dineshjangra0062@gmail.com>
2026-01-26 14:12:35 +0530

    practices

---
+++ examples.py
@@ -0,0 +1,14 @@
+print("testing")
+lines = int(input("enter the how many number of lines: "))
+lines +=1
+for i in range (lines):
+    star= "*"
+    for j in range (lines):
+        print(star*j)
+        print(star*lines)
+    
+    lines-=1
+   
+    
+    
+    
